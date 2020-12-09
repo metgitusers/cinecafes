02376ed4147ca922b87157a06e87659fe3d81a5d
metgituser <gituser@met-technologies.com>
2020-12-09 16:20:44 +0530

    fifth Commit

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
                     	<img src="assets/img/banner.jpg" width="100%" alt=""/>
                     	<div class="overlay">
                     		<h2>The Amalgamation of</h2>
-                    		<h4>Hospitality  & Entertainment</h4>
+                    		<h4>Hospitality <br> & <br> Entertainment</h4>
                     	</div>
                     </div>
                     <div class="item">
