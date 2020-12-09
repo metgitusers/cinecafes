a06d0e3118fc3a6c10cc93f1a5bcf57e1d5c42a7
metgituser <gituser@met-technologies.com>
2020-12-09 14:30:25 +0530

    fifth Commit

--- index.php
+++ index.php
@@ -624,28 +624,6 @@ The customer has an option to cancel online on cinecafes mobile App on the terms
   </div>
 </div>
 
-	<?php
-	  $status=$_GET['status'];
-	
-	if($status=="success"){
-	?>
-	<div class="modal fade" id="be_franshise_panel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Be a Franchise</h4>
-		<span id="msg_succ"></span>
-		<span id="msg_err"></span>
-      </div>
-      <div class="modal-body" id="contact_page">
-            <p>Form Submitted Successfully.</p>
-      </div>
-      
-    </div>
-  </div>
-</div>
-	<?php } ?>
 <!-- Bootstrap core JavaScript --> 
 <script src="assets/js/jquery.1.11.3.min.js"></script> 
 <script src="assets/js/bootstrap.min.js"></script> 
