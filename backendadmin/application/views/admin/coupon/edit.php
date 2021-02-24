a545f2c26db35a36395381e5bf3fd198fb7e4d22
metgituser <gituser@met-technologies.com>
2021-02-24 17:14:06 +0530

    update copupon

--- backendadmin/application/views/admin/coupon/edit.php
+++ backendadmin/application/views/admin/coupon/edit.php
@@ -72,13 +72,13 @@
                 <div class="col-md-4 col-sm-12 col-xs-12  mt-3">
                   <div class="form-group">
                      <label>Min Purchase Amount*</label>
-                       <input class="form-control" type="number" min="0" name="min_price" id="min_price"  value="<?php echo $row['min_price'];?>"> 
+                       <input class="form-control" type="number" min="0" name="min_price" id="min_price" required  value="<?php echo $row['min_price'];?>"> 
                     </div>
                 </div>
                  <div class="col-md-4 col-sm-12 col-xs-12 max-discount-percentage  mt-3" style="display: <?= $row['coupon_type']==1?'':'none'?> ">
                   <div class="form-group">
-                     <label>Max Discount Amount</label>
-                       <input class="form-control" type="number" min="1" name="max_discount_amount" id="max_discount_amount"  value="<?php echo $row['max_discount_amount'];?>"> 
+                     <label>Max Discount Amount*</label>
+                       <input class="form-control" type="number" min="1" name="max_discount_amount" id="max_discount_amount" <?= $row['coupon_type']==1?'required':''?>  value="<?php echo $row['max_discount_amount'];?>"> 
                     </div>
                 </div>
                 <script>
