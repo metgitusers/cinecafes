8a14655df28ef593c3025ddd715f3e61f308b7ef
metgituser <gituser@met-technologies.com>
2021-02-24 17:03:23 +0530

    update resolved bugs

--- backendadmin/application/controllers/admin/Transactionhistory.php
+++ backendadmin/application/controllers/admin/Transactionhistory.php
@@ -33,7 +33,7 @@ class Transactionhistory extends MY_Controller {
         $data['user_id']=$user_id; 
        
 		$data['list']=$this->mtransactionhistory->getTransactionhistoryList($start_date,$end_date,$user_id);
-		//echo $this->db->last_query();
+		echo $this->db->last_query();
 		$data['user_list'] =$this->mtransactionhistory->getDistinctUser();
 		//echo '<pre>';
 		//print_r($data['list']); die;
--- backendadmin/application/models/admin/Mtransactionhistory.php
+++ backendadmin/application/models/admin/Mtransactionhistory.php
@@ -39,7 +39,7 @@ class Mtransactionhistory extends CI_Model{
        
         if(!empty($start_date) && !empty($end_date) ){
            //$this->db->where('reservation_date between "'.$start_dt.'" and "'.$end_dt.'"');
-           $this->db->where('transaction_date between "'.date('Y-m-d h:i:s', strtotime($start_date)).'" and "'.date('Y-m-d 23:59:00', strtotime($end_date)).'"');
+           $this->db->where('transaction_date between "'.date('Y-m-d 00:00:01', strtotime($start_date)).'" and "'.date('Y-m-d 23:59:00', strtotime($end_date)).'"');
         }
         if($user_id > 0){
            //$this->db->where('reservation_date between "'.$start_dt.'" and "'.$end_dt.'"');
