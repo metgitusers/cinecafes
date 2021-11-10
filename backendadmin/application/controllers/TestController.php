0ea89354cc625b65eb766bca31de84126211132d
metgituser <gituser@met-technologies.com>
2021-11-10 16:37:48 +0530

    gg

--- backendadmin/application/controllers/TestController.php
+++ backendadmin/application/controllers/TestController.php
@@ -11,5 +11,33 @@ class TestController extends MY_Controller {
 	public function index(){
 		echo base_url();
  	}
+	
+	public function testEmail()
+	{
+		$email_to='santu.dutta@met-technologies.com';
+		
+		$subject = 'Contact us mail received - 52889566656333';
+		$message = '<div style="border:2px solid #333;padding:20px;background:#eee;text-align:center;">
+					<img src="'.LOGOURL.'"/ style="display:block;">
+					<p>Contact us mail - 52889566656333<br>
+					Someone has contacted you.Please login to your admin panel to see the details.</p>
+					
+					<p>Cinecafe,
+					This message was sent to '.$email_to.' and intended for your account. Not your account? Remove your email from this account.</p>
+					</div>';
+					
+					
+		$mail['name']     = 'Santu dutta';
+        $mail['to']       = $email_to;
+        $mail['subject']  = 'cinecafe Recover Password';
+		
+		$mail['message']    = $message;
+        $mail['from_email']    = FROM_EMAIL;
+        $mail['from_name']    = ORGANIZATION_NAME;
+		
+		
+		sendmail($mail);
+		show_error($this->email->print_debugger());
+	}
 }
 ?>
