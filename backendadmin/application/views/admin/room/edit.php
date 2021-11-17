e27b94158591bb3ef3a4a2cd86da11ac1aa46430
metgituser <gituser@met-technologies.com>
2021-11-17 13:00:07 +0530

    gg

--- backendadmin/application/views/admin/room/edit.php
+++ backendadmin/application/views/admin/room/edit.php
@@ -123,7 +123,7 @@
                   <div class="form-group">
                     <input type="hidden" name="old_image" value="<?php echo $row['image'];?>">
                     <input type="hidden" name="room_id" value="<?php echo $row['room_id'];?>">
-                     <button type="submit" class="btn btn-primary btn-user btn-block">Upadte</button>
+                     <button type="submit" class="btn btn-primary btn-user btn-block">Update</button>
                         <!--  <input type="submit" name="submit" value="Submit"/> -->
                      </div>
                 </div>
