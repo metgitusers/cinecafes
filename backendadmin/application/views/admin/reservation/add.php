376e5f63b04ba2ccdc4e270ba8d6f2f84515d20f
metgituser <gituser@met-technologies.com>
2021-11-20 22:45:30 +0530

    footer upload

--- backendadmin/application/views/admin/reservation/add.php
+++ backendadmin/application/views/admin/reservation/add.php
@@ -282,11 +282,11 @@ var placeSearch, autocomplete;
     $('button[type="submit"]').on('click', function() {
       
       var getTime = $('#reservation_time').timepicker('getTime');
-      var getHoursC = getTime.getHours();
-      var getMinutesC = getTime.getMinutes();
+      var getHours = getTime.getHours();
+      var getMinutes = getTime.getMinutes();
       
-      //var totalMinutesC = parseInt(getHoursC)*60+parseInt(getMinutesC);
-      alert(getHoursC);alert(getMinutesC);alert(totalMinutesC);
+      var totalMinutes = parseInt(getHours)*60+parseInt(getMinutes);
+      alert(getHours);alert(getMinutes);alert(totalMinutes);
       
       //var reservation_date,reservation_time,reservation_date_time;
       //
