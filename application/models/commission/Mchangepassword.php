fd8620a451ed80527a8e8bf09cdd1d2ba385681d
metgituser <gituser@met-technologies.com>
2022-02-27 23:50:25 +0530

    changes made

--- application/models/commission/Mchangepassword.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Mchangepassword extends CI_Model {
-
-    
-
-
-}
--- application/views/commission/layouts/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->load->view('admin/layouts/header');
-$this->load->view($content);
-$this->load->view('admin/layouts/footer');
-?>
