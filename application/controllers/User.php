0ea03e5b8e7db5023bafc6fd36bff197c3a221af
metgituser <gituser@met-technologies.com>
2022-03-04 13:54:32 +0530

    google captcha upload

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,7 @@ class User extends MY_Controller {
 
 	public function __construct() {		
 		parent::__construct();
-		//echo "1";exit;
+
 		//$this->redirect_guest_user();
 		
 		$this->load->model('mcommon');	
@@ -25,7 +25,7 @@ class User extends MY_Controller {
     	$data['list']= $this->mcommon->getDetails('banner',['status'=> 1, 'is_delete'=> 0]);
     	$data['home_section']= $this->mcommon->getDetails('content',['status'=> 1, 'is_delete'=> 0]);
     	//print_r($data['content']);die;
-	   	$data['title']='cinecafe | Home';
+	   	$data['title']='cinecafe2 | Home';
 		$data['content']='front/pages/index';
 		$this->front_load_view($data);
 	}
@@ -34,7 +34,7 @@ class User extends MY_Controller {
 	{
 		$data['list'] 	= $this->mcommon->getDetails('cms',['cms_slug'=>'about-us' ,'status' =>1]);
 		//print_r($data['list']);die;
-		$data['title']='cinecafe | About Us';
+		$data['title']='cinecafe2 | About Us';
 		$data['content']='front/pages/about-us';
 		$this->front_load_view($data);
 	}
@@ -43,7 +43,7 @@ class User extends MY_Controller {
 	{
 		$data['list'] 	= $this->mcommon->getDetails('cms',['cms_slug'=>'terms-condition' ,'status' =>1]);
 		//print_r($data['list']);die;
-		$data['title']='cinecafe | Terms & Condition';
+		$data['title']='cinecafe2 | Terms & Condition';
 		$data['content']='front/pages/about-us';
 		$this->front_load_view($data);
 	}
@@ -52,7 +52,7 @@ class User extends MY_Controller {
 	{
 		$data['list'] 	= $this->mcommon->getDetails('cms',['cms_slug'=>'privacy-policy' ,'status' =>1]);
 		//print_r($data['list']);die;
-		$data['title']='cinecafe | Privacy - Policy';
+		$data['title']='cinecafe2 | Privacy - Policy';
 		$data['content']='front/pages/about-us';
 		$this->front_load_view($data);
 	}
@@ -61,78 +61,90 @@ class User extends MY_Controller {
 	{
 		if($this->input->method() == 'post'):
 			$postData = $this->input->post();
-			$data = [
-							'first_name' => $postData['first_name'],
-							'last_name' => $postData['last_name'],
-							'email' => $postData['email'],
-							'phone_no' => $postData['phone_no'],
-							'message' => $postData['message']
-					];	
+		 	$recaptchaResponse = trim($this->input->post('g-recaptcha-response'));
+		 	if(!empty($recaptchaResponse)){
+	         	$userIp=$this->input->ip_address();
+	         	$secret = $this->config->item('google_secret');
+	        	$url="https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$recaptchaResponse."&remoteip=".$userIp;
+		        $ch = curl_init(); 
+		        curl_setopt($ch, CURLOPT_URL, $url); 
+		        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		        $output = curl_exec($ch); 
+		        curl_close($ch);  
+	        	$cap_status= json_decode($output, true);
+	        	if(!empty($cap_status)){
+		        	if($cap_status['success']) {
+		        		$data = array(
+									'first_name' => $this->input->post('first_name'),
+									'last_name' => $this->input->post('last_name'),
+									'email' => $this->input->post('email'),
+									'phone_no' => $this->input->post('phone_no'),
+									'message' => $this->input->post('message')
+							);	
 
-			$this->mcommon->insert('contact',$data);
-			/*$data['first_name'] =$first_name;
-			$data['last_name'] =$last_name;
-			$data['email'] =$email;
-			$data['phone_no'] =$phone_no;
-			$data['message'] =$msg;*/
+						$this->mcommon->insert('contact',$data);
+						//$email_to='sourav.bhowmick@met-technologies.com';
+						
+						//$email_to='avishek.chakraborty@met-technologies.com';
+						$email_to='franchise.enquiry@cinecafes.com';
 
+						$subject = 'Contact us mail received';
+						$message = '<div style="border:2px solid #333;padding:20px;background:#eee;text-align:center;">
+							<img src="'.LOGOURL.'"/ style="display:block;">
+							<p>Contact us mail<br>
+							Someone has contacted you.Please find Out Below Enquiry.</p><br>
 
-			$email_to='franchise.enquiry@cinecafes.com';
-		$subject = 'Contact us mail received';
-		$message = '<div style="border:2px solid #333;padding:20px;background:#eee;text-align:center;">
-					<img src="'.LOGOURL.'"/ style="display:block;">
-					<p>Contact us mail<br>
-					Someone has contacted you.Please find Out Below Enquiry.</p><br>
-
-					<table cellpadding="10" cellspacing="0" width="100%" style="border: 1px solid #ccc">
-                     <tr>
-                        <td>First Name</td>
-                        <td>'.$postData['first_name'].'</td>
-                     </tr>
-                     <tr>
-                        <td>Last Name</td>
-                        <td>'.$postData['last_name'].'</td>
-                     </tr>
-                     <tr>
-                        <td>Email</td>
-                        <td>'.$postData['email'].'</td>
-                     </tr>
-                     <tr>
-                        <td>Phone No</td>
-                        <td>'.$postData['phone_no'].'</td>
-                     </tr>
-                     <tr>
-                        <td>Message</td>
-                        <td>'.$postData['message'].'</td>
-                     </tr>
-                  </table><br>
-					
-					<p>Thanks
-					 & Regards,Cinecafe</p>
-					</div>';
-					
-		$mail['name']     = 'Sourav Bhowmik';
-        $mail['to']       = $email_to;
-        $mail['subject']  = $subject;
-		$mail['message']  = $message;
-		
-		sendmail($mail);
-		//show_error($this->email->print_debugger());
-			/*$html2 = $this->load->view('email_template/contact-mail',$data,TRUE);
-			//$details = $this->mcommon->getRow('user_master',['role_id'=>1]);
-			$responseMail = $this->sendMail('franchise.enquiry@cinecafes.com','Contact Enquiry Mail',$html2);*/
-			$response['status']= TRUE;
-			$response['message']= 'Your enquiry successfully submitted!';
-			$response['redirect']= 'User';
-			$this->outputJson($response);
-            /*$this->session->set_flashdata('success_message', 'Your enquiry successfully submitted');
-            redirect(base_url().'contact-us');*/
-
-
-
-
-
-			
+							<table cellpadding="10" cellspacing="0" width="100%" style="border: 1px solid #ccc">
+		                     <tr>
+		                        <td>First Name</td>
+		                        <td>'.$postData['first_name'].'</td>
+		                     </tr>
+		                     <tr>
+		                        <td>Last Name</td>
+		                        <td>'.$postData['last_name'].'</td>
+		                     </tr>
+		                     <tr>
+		                        <td>Email</td>
+		                        <td>'.$postData['email'].'</td>
+		                     </tr>
+		                     <tr>
+		                        <td>Phone No</td>
+		                        <td>'.$postData['phone_no'].'</td>
+		                     </tr>
+		                     <tr>
+		                        <td>Message</td>
+		                        <td>'.$postData['message'].'</td>
+		                     </tr>
+		                  </table><br>
+							
+							<p>Thanks
+							 & Regards,Cinecafes</p>
+							</div>';
+							
+						$mail['name']     = 'Cinecafes';
+				        $mail['to']       = $email_to;
+				        $mail['subject']  = $subject;
+						$mail['message']  = $message;
+						sendmail($mail);
+						$response['status']= TRUE;
+						$response['message']= 'Your enquiry successfully submitted!';
+						$response['redirect']= 'User';
+			        }else{
+			            $response['status']= FALSE;
+						$response['message']= 'Sorry Google Recaptcha Unsuccessful!!';
+						$response['redirect']= '';
+			        }
+			    }else{
+			    	$response['status']= FALSE;
+					$response['message']= 'Something Went Wrong!!';
+					$response['redirect']= '';
+			    }
+			}else{
+				$response['status']= FALSE;
+				$response['message']= 'Please check Google Recaptcha!!';
+				$response['redirect']= '';
+			}
+			$this->outputJson($response);	
 		endif;
 	}
 
@@ -140,7 +152,7 @@ class User extends MY_Controller {
 	public function gallery()
 	{
 		$data['home_section']= $this->mcommon->getDetails('content',['status'=> 1, 'is_delete'=> 0]);
-		$data['title']='cinecafe | Gallery';
+		$data['title']='cinecafe2 | Gallery';
 		$data['content']='front/pages/gallery';
 		$this->front_load_view($data);
 	}
